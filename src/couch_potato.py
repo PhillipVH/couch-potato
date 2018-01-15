ee3eb65ec5083cc53906cf644bdea08d97b7ec9a
Phillip van Heerden <vanheerden.phillip@gmail.com>
2018-01-15 16:40:01 +0000

    Added random movie select functionality ...
    Internally Couch Potato now uses OptionParser callbacks to react to command line flags.

--- src/couch_potato.py
+++ src/couch_potato.py
@@ -1,3 +1,5 @@
+import random
+
 import pygsheets
 
 from optparse import OptionParser
@@ -24,22 +26,54 @@ def authorize_and_get_sheet():
     return sheet
 
 
+def get_entire_library():
+    # Obtain a handle to the sheet
+    movie_sheet = authorize_and_get_sheet()
+
+    # Return all rows except the first "header" row
+    return movie_sheet.get_all_values(returnas="matrix")[1:]
+
+
+def add_movie_callback(option, opt, value, parser):
+    # Obtain a handle to the sheet
+    movie_sheet = authorize_and_get_sheet()
+
+    # Insert the movie into the library
+    movie_sheet.insert_rows(row=1, number=1, values=[value, 0, 0, 0])
+
+    print(f"Inserted \"{value}\"! Get to watching!")
+
+
+def get_random_movie_callback(option, opt, value, parser):
+    print("Random movie, coming right up!")
+
+    # Grab the library
+    library = get_entire_library()
+
+    # Sample a random movie...
+    random_index = random.randint(0, len(library) - 1)
+
+    print(f"How do you feel about \"{library[random_index][0]}\"?")
+
+
 def main():
     usage = "usage: %prog [options] arg"
     parser = OptionParser(usage)
 
     # Option to add a new movie to the library
-    parser.add_option("-a", "--add-movie", dest="movie_name",
+    parser.add_option("-a", "--add-movie",
+                      action="callback",
+                      callback=add_movie_callback,
+                      type="str", nargs=1, dest="movie_name",
                       help="Add a new movie with the given title")
 
+    # Option to select a random movie from the library
+    parser.add_option("-r", "--random",
+                      action="callback",
+                      callback=get_random_movie_callback,
+                      help="Selects a random movie")
+
     (options, args) = parser.parse_args()
-
-    # Obtain a handle to the sheet
-    movie_sheet = authorize_and_get_sheet()
-
-    # Insert the movie into the library
-    movie_sheet.insert_rows(row=1, number=1, values=[options.movie_name, 0, 0, 0])
-    print(f"Inserted {options.movie_name}! Get to watching!")
 
 
 if __name__ == "__main__":
